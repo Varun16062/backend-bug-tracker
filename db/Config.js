f4f5aba44d771ba93555cf3112dc3d9a170eab56
Varun16062 <153092203+Varun16062@users.noreply.github.com>
2025-06-23 22:13:31 +0530

    bnackend

--- db/Config.js
+++ db/Config.js
@@ -1,6 +1,6 @@
 // Database connection
 require('dotenv').config();
 const mongoose = require('mongoose');
-const URL = process.env.MONGO_URL ;
-
-mongoose.connect(process.env.MONGO_URI);
+const URL = process.env.MONGO_URI ;
+console.log(URL)
+mongoose.connect(URL);
